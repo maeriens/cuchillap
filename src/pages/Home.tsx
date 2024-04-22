833bb87aa7f99200b7f677dd229fda66e8ca42a5
Mariano Perdomo <perdomo.mariano@gmail.com>
2024-04-22 01:23:57 -0300

    Sharee search between tabs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import { Box, Container, Tabs } from "@radix-ui/themes";
+import { Cross1Icon } from "@radix-ui/react-icons";
+import { Box, Container, Flex, Tabs, TextField } from "@radix-ui/themes";
+import { ChangeEvent, useState } from "react";
 
 import { IngredientsIndividualTable } from "../components/IngredientsIndividualTable/Table";
 import { IngredientsTotalTable } from "../components/IngredientsTotalTable/Table";
-import Tab from "../components/Tab";
+import { useDebounce } from "../hooks/useDebounceValue";
 
 const TABS_DATA = [
   {
@@ -15,25 +17,48 @@ const TABS_DATA = [
   },
 ];
 
-const TABS_TRIGGERS = TABS_DATA.map((t) => t.name);
-
 function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const debouncedTerm = useDebounce(searchTerm, 350);
+
+  const handleSearch = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(target?.value ?? "");
+  };
+
+  const clearSearch = () => setSearchTerm("");
+
   return (
     <Container>
-      <Tabs.Root defaultValue="total">
+      <Tabs.Root defaultValue={TABS_DATA[0].name}>
         <Tabs.List>
-          {TABS_TRIGGERS.map((tabName) => (
-            <Tabs.Trigger value={tabName} className="capitalize">
-              {tabName}
+          {TABS_DATA.map(({ name }) => (
+            <Tabs.Trigger value={name} className="capitalize" key={name}>
+              {name}
             </Tabs.Trigger>
           ))}
         </Tabs.List>
 
         <Box pt="3">
           {TABS_DATA.map((tab) => (
-            <Tab tabName={tab.name}>
-              {(searchTerm: string) => <tab.Component searchTerm={searchTerm} />}
-            </Tab>
+            <Tabs.Content value={tab.name}>
+              <Box position="sticky" top="0" style={{ zIndex: 2, background: "#FFF" }} pb="4">
+                <TextField.Root
+                  placeholder="Buscá si te pinta…"
+                  radius="large"
+                  onChange={handleSearch}
+                  value={searchTerm}
+                >
+                  <TextField.Slot side="right">
+                    {searchTerm.length > 0 && (
+                      <Cross1Icon height="16" width="16" onClick={clearSearch} />
+                    )}
+                  </TextField.Slot>
+                </TextField.Root>
+              </Box>
+              <Flex direction="column">
+                <tab.Component searchTerm={debouncedTerm} />
+              </Flex>
+            </Tabs.Content>
           ))}
         </Box>
       </Tabs.Root>
